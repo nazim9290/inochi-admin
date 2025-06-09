1f778f73902b9b701b0ad011dfd2a622ca1f524a
md nazim uddin <nazim9290@gmail.com>
2025-06-09 18:04:15 +0900

    update

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
-const dev = process.env.NODE_ENV !== "production";
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const port = !dev ? process.env.PORT : 3000;
+app.use(express.static(path.join(__dirname, "build")));
 
-// Create the Express-Next App
-const app = next({ dev });
-const handle = app.getRequestHandler();
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-app
-  .prepare()
-  .then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
-      handle(req, res, parsedUrl);
-      console.log("pathname", pathname);
-    }).listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-  })
-  .catch((ex) => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
